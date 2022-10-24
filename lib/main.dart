1bc4ad1b08552c2025e02d8ba5cd5db70f211482
nguyenduong <nguyenduong0841999@gmail.com>
2022-10-24 18:28:41 +0700

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,91 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+  Completer<WebViewController> _controller =
+  Completer<WebViewController>();
+
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: WebView(
+        initialUrl: 'https://m.facebook.com/',
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (webViewController) =>
+        {
+          _controller.complete(webViewController)
+        },
+        javascriptChannels: <JavascriptChannel>{
+          _toasterJavascriptChannel(context),
+        },
+        onPageFinished: (String url) async {
+          final String cookies =
+          await _controller.future.then((value)  {
+
+            value.runJavascriptReturningResult('document.cookie').then((value2) => print('Cookie $value2'));
+            return "" ;
+          });
+          if(cookies.contains("c_user"))
+            print("Đăng nhập thành công");
+          print('Cookie $cookies');
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+
+  }
+  JavascriptChannel _toasterJavascriptChannel(BuildContext context) {
+    return JavascriptChannel(
+        name: 'Toaster',
+        onMessageReceived: (JavascriptMessage message) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(content: Text(message.message)),
+          );
+        });
+  }
+}
